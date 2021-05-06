be61b2cfdf8c36caf96df449c5632b1961e87468
noahlinn <n.linn95@gmail.com>
2021-05-06 10:53:40 -0400

    user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,17 +6,83 @@ const userController = {}
 
 userController.creatUser = async (req, res) =>{
     try {
-       
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+
         const user = await models.user.create({
             name: req.body.name,
             email:req.body.email,
             password: req.body.password
         })
+        const encryptedId = jwt.sign({ userId: user.id }, process.env.JWT_SECRET)
         res.json({user})
     } catch (error) {
         console.log(error)
         res.json({error})
     }
 }
+
+userController.login = async (req,res) => {
+    try {
+        const user = await models.user.findOne({
+            where:{
+                email: req.body.email
+            }
+        })
+        if (bcrypt.compareSync(req.body.password, user.password)){
+            const encryptedId = jwt.sign({ userId: user.id }, process.env.JWT_SECRET)
+            res.json({user, encryptedId})
+        }
+        else{
+            res.status(401).json({message: 'login failed'})
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+        console.log(error)
+    }
+}
+
+userController.verify = async (req, res) => {
+    try {
+        const encryptedId = req.headers.authorization
+        const decryptedId = await jwt.verify(encryptedId, process.env.JWT_SECRET)
+        
+        const user = await models.user.findOne({
+            where:{
+                id: decryptedId.userId
+            }
+        })
+        if(user){
+            res.json({user})
+        }
+        else{
+            res.status(404).json({message: 'user not found'})
+            console.log(error)
+        }
+    } catch (error) {
+        res.json({error})
+        console.log(error)
+    }
+}
+
+userController.getUserInfo = async (req, res) => {
+    try{
+        const encryptedId = req.headers.authorization
+        const decryptedId = await jwt.verify(encryptedId, process.env.JWT_SECRET)
+        const user = await models.user.findOne({
+            where: {
+                id: decryptedId.userId
+            }
+        })
+        res.json({
+            user: user
+        })    
+    }
+    catch (error) {
+        res.json({
+            error
+        })
+    }
+}
+
 
 module.exports = userController; 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const userRoutes = require('express').Router()
 const userController = require('../controllers/userController')
 
-
+userRoutes.post('/login', userController.login)
 
 module.exports = userRoutes; 
