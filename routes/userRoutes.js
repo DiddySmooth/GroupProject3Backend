1db6dda381580d61b4ae497311b854e2d32188fb
noahlinn <n.linn95@gmail.com>
2021-05-06 10:59:06 -0400

    user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,5 +2,7 @@ const userRoutes = require('express').Router()
 const userController = require('../controllers/userController')
 
 userRoutes.post('/login', userController.login)
+userRoutes.post('/create', userController.login)
+
 
 module.exports = userRoutes; 
